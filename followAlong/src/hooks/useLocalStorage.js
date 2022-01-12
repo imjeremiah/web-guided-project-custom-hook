2e5fb6e4a6b5b62282f4ff9b85054b0e3c671189
Warren C. Longmire <warren.longmire@lambdaschool.com>
2022-01-12 14:06:47 -0500

    lecture work-in-progress

--- followAlong/src/hooks/useLocalStorage.js
+++ followAlong/src/hooks/useLocalStorage.js
@@ -9,7 +9,7 @@ const useLocalStorage = (key, initValue) => {
   
     const [ value, setValue] = useState(()=> {
       if (localStorage.getItem(key)) {
-        return(localStorage.getItem(key))
+        return(JSON.parse(localStorage.getItem(key)))
       } else {
         localStorage.setItem(key, initValue);
         return(initValue);

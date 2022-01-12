edf509557e9655e1c2faba80c77f3b9d8d817290
Warren C. Longmire <warren.longmire@lambdaschool.com>
2022-01-12 12:22:47 -0500

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -20,27 +20,8 @@ function App() {
 
   return (
     <div className="App">
-      <div id="selectedDiv">
-        <h2>Selected Pokemon: {selectedPokemon.name || "none"}</h2>
-        {selectedPokemon.name && (
-          <div>
-            <img
-              src={selectedPokemon.sprites.front_default}
-              alt={selectedPokemon.sprites.front_default}
-            />
-            <h5>Height: {selectedPokemon.height}</h5>
-            <h5>Weight: {selectedPokemon.weight}</h5>
-            <h5>Abilities:</h5>
-            <ul>
-              {selectedPokemon.abilities.map((a) => (
-                <li key={a.ability.name}>
-                  {a.ability.name} - Slot {a.slot}{" "}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-      </div>
+      <SelectedPoke />
+
       <div id="pokeList">
         {pokemen.map((pokemon) => (
           <div

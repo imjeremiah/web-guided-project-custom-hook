682522c310c9baaea8d530fa8edf9a8dc58ee669
Warren C. Longmire <warren.longmire@lambdaschool.com>
2022-01-12 12:29:54 -0500

    lecture work-in-progress

--- review/src/App.js
+++ review/src/App.js
@@ -5,27 +5,6 @@ import data from "../data";
 
 import SelectedPoke from './components/SelectedPoke';
 
-const PokeList = (props) => {
-  const {pokemen, handlePoke} = props;
-
-  return (<div id="pokeList">
-    {pokemen.map((pokemon) => (
-      <div
-        onClick={() => handlePoke(pokemon.id)}
-        key={pokemon.id}
-        className="pokemon"
-      >
-        <img src={pokemon.img} alt={pokemon.name} />
-        <div>
-          <h3>{pokemon.name}</h3>
-          {pokemon.next_evolution &&
-            pokemon.next_evolution.map((e) => <p key={e.num}>{e.name}</p>)}
-        </div>
-      </div>
-    ))}
-  </div>);
-}
-
 function App() {
   const [pokemen, setPokemen] = useState([]);
   const [selectedPokemon, setSelectedPokemon] = useState({});

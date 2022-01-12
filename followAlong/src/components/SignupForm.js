a98d29405781475d49f8827a18c7e377579d5747
Warren C. Longmire <warren.longmire@lambdaschool.com>
2022-01-12 14:04:25 -0500

    lecture work-in-progress

--- followAlong/src/components/SignupForm.js
+++ followAlong/src/components/SignupForm.js
@@ -24,30 +24,6 @@ const useStyles = makeStyles(theme => ({
 }));
 
 
-const useLocalStorage = (key, initValue) => {
-  //useLocalStorage
-  //1. when we set our initialValue check to see if we have saved a localStorage value
-  //2. if that localStorage value exists: set initial value to localStorage value
-  //3. if it does not exist: set initial value initValue && localStorage value initValue
-  //4. Any time we setState, also set localStorage
-
-  const [ value, setValue] = useState(()=> {
-    if (localStorage.getItem(key)) {
-      return(localStorage.getItem(key))
-    } else {
-      localStorage.setItem(key, initValue);
-      return(initValue);
-    }
-  });
-
-  const setStoredValue = (newValue) => {
-    localStorage.setItem(key, newValue);
-    setValue(newValue);
-  }
-
-  return[value, setStoredValue];
-}
-
 export default function SignupForm() {
   const classes = useStyles();
 

34cde8da09287b2536ce96bb2fd0d97c59e09748
Warren C. Longmire <warren.longmire@lambdaschool.com>
2022-01-12 13:36:49 -0500

    lecture work-in-progress

---
+++ followAlong/src/hooks/useForm.js
@@ -0,0 +1,24 @@
+//Create useForm Custom Hook:
+//1. Make a function called useForm.
+//2. Add in all stateful logic into this hook.
+//3. Return all needed values from this hook.
+//4. Replace useState with useForm.
+const useForm = (initName) => {
+    const [values, setValue] = useState(initName);
+  
+    const handleChanges = e => {
+      setValue({
+        ...values,
+        [e.target.name]: e.target.value
+      });
+    };
+  
+    const clearForm = e => {
+      e.preventDefault();
+      setValue(initName);
+    };
+  
+    return([values, handleChanges, clearForm]);
+}
+
+export default useForm;
